89f185c37c56e584ec08a2adda71f88a9fc189e0
unknown <gaviriayeferson2@gmail.com>
2025-04-09 10:07:16 +0200

    fix: Modify ImageController to call VisionService during image upload and save returned label

--- src/main/java/dev/yeferson/tu_estilo_nube_BE/image/ImageService.java
+++ src/main/java/dev/yeferson/tu_estilo_nube_BE/image/ImageService.java
@@ -1,7 +1,6 @@
 package dev.yeferson.tu_estilo_nube_BE.image;
 
 import dev.yeferson.tu_estilo_nube_BE.user.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,15 +9,26 @@ import java.util.Optional;
 @Service
 public class ImageService {
 
-    @Autowired
-    private ImageRepository imageRepository;
+    private final ImageRepository imageRepository;
 
-    public Image saveImage(String fileName, byte[] data, User user) {
+    // Inyección de dependencias mediante constructor
+    public ImageService(ImageRepository imageRepository) {
+        this.imageRepository = imageRepository;
+    }
+
+    // Método para guardar la imagen con etiquetas
+    public Image saveImage(String fileName, byte[] data, User user, List<String> labels) {
         Image image = new Image();
         image.setFileName(fileName);
         image.setData(data);
         image.setUser(user);
+        image.setLabels(labels);
         return imageRepository.save(image);
+    }
+
+    // Método original que delega al nuevo método si no se proporcionan etiquetas
+    public Image saveImage(String fileName, byte[] data, User user) {
+        return saveImage(fileName, data, user, null);
     }
 
     public List<ImageDTO> findByUser(User user) {
